fe7f052635f005b0a941d23b9c8147206d1cd4b9
Marcus Heneborn <heneborn@gmail.com>
2024-01-09 12:35:11 +0100

    Create thank_you.php

---
+++ thank_you.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Booking - Thank You</title>
+</head>
+<body>
+    <h1>Thank You for Booking!</h1>
+    <p>Your booking has been confirmed.</p>
+</body>
+</html>
